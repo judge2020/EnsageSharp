f688df0b01867e61eec23ab98a439966e04ebce7
judge2020 <hunterray55@hotmail.com>
2016-07-28 21:39:36 -0400

    temp fix 2 :/

--- AbaddonDeny/Program.cs
+++ AbaddonDeny/Program.cs
@@ -8,7 +8,8 @@ namespace AbaddonDeny
 {
     internal class Program
     {
-        private static Hero me;
+        private static Hero _me;
+        private static string _isalived;
         private static readonly Menu Menu = new Menu("Abaddon Deny", "abaddondeny", true, "npc_dota_hero_abaddon", true); //test commit 1
         private static void Main()
         {
@@ -19,12 +20,16 @@ namespace AbaddonDeny
         }
         public static void Game_OnUpdate(EventArgs args)
         {
-            me = ObjectManager.LocalHero;
-            if (me.IsAlive == false) return;
-            if (!Game.IsInGame || me.ClassID != ClassID.CDOTA_Unit_Hero_Abaddon) return;
+            _isalived = "yes";
+            _me = ObjectManager.LocalHero;
+            if (_me.IsAlive == false)
+            {
+                _isalived = "no";
+            }
+            if (!Game.IsInGame || _me.ClassID != ClassID.CDOTA_Unit_Hero_Abaddon) return;
             if (!Menu.Item("toggle").GetValue<bool>()) return;
-            var q = me.Spellbook.SpellQ;
-            var qlvl = me.Spellbook.SpellQ.Level;
+            var q = _me.Spellbook.SpellQ;
+            var qlvl = _me.Spellbook.SpellQ.Level;
             var qdmg = 0;
             if (qlvl <= 0)
                 qdmg = 0;
@@ -36,15 +41,18 @@ namespace AbaddonDeny
                 qdmg = 125;
             if (qlvl == 4)
                 qdmg = 150;
-            if (qdmg <= me.Health && me.Health != 1 && me.Health != 0) return;
+            if (qdmg <= _me.Health && _me.Health != 1 && _me.Health != 0) return;
             
             var closestUnit = ObjectManager.GetEntities<Unit>()
-                .Where(x =>(!x.Equals(me) && ((x is Hero && !x.IsIllusion) || (x is Creep && x.IsSpawned)) && x.IsAlive && x.IsVisible))
+                .Where(x =>(!x.Equals(_me) && ((x is Hero && !x.IsIllusion) || (x is Creep && x.IsSpawned)) && x.IsAlive && x.IsVisible))
                 .OrderBy(x => x.Distance2D(ObjectManager.LocalHero))
                 .FirstOrDefault();
             if (ObjectManager.LocalHero.Modifiers.Any(modifier => modifier.Name.Contains("modifier_abaddon_aphotic_shield"))) return;
-            q.UseAbility(closestUnit);
 
+            if (_isalived == "yes")
+            {
+                q.UseAbility(closestUnit);
+            }
         }
     }
 }
